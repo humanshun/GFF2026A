5216217bb0471f8da0ca1890b6fec00a4aae0b9e
Kitahara7023 <human.kagoshima.naozumi@gmail.com>
2025-09-19 16:38:23 +0900

    [add]playername追加

---
+++ GFF2026A/Assets/Scripts/UI/UIDeme.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Text.RegularExpressions;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Windows;
+
+public class UIDeme : MonoBehaviour
+{
+    public TextMeshProUGUI output;
+    public TMP_InputField userName;
+
+    private string allowedPattern = @"^[ぁ-んァ-ン一-龥a-zA-Z0-9]+$";
+    public void ButtonDemo()
+    {
+        string Input = userName.text;
+        Input = Input.Trim();
+        Input = Input.Trim();                      // 前後のスペース削除
+        Input = Input.Replace(" ", "");            // 半角スペース削除
+        Input = Input.Replace("　", "");           // 全角スペース削除
+
+        if (Regex.IsMatch(Input,allowedPattern))
+        {
+            output.text = Input;
+        }
+        else
+        {
+            output.text = "名前は半角英数字と日本語で2～12文字以内にしてください";
+
+        }
+    }
+}
