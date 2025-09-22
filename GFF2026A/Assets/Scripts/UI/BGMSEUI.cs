72327c51f05df2dbfbad64be018682ab7d2ca052
kuzuboshi <Human.kagoshima.IdeRyouta@gmail.com>
2025-09-22 12:58:11 +0900

    [add] jsonファイル

--- GFF2026A/Assets/Scripts/UI/BGMSEUI.cs
+++ GFF2026A/Assets/Scripts/UI/BGMSEUI.cs
@@ -3,40 +3,32 @@ using UnityEngine.UI;
 
 public class BGMSEUI : MonoBehaviour
 {
+
     // BGMのスライダー
-    private Slider bgmSlder;
+    private Slider bgmSlider;
 
     // SEのスライダー
     private Slider seSlider;
 
+    private Soundjson soundjson;
+
     void Start()
     {
-        // Sliderコンポーネントの取得
-        bgmSlder = GetComponent<Slider>();
+        soundjson = FindAnyObjectByType<Soundjson>();
+        // bgmSliderコンポーネントの取得
+        bgmSlider = GetComponent<Slider>();
 
-        // Sliderコンポーネントの取得
+        // seSliderコンポーネントの取得
         seSlider = GetComponent<Slider>();
-
-        // valueを初期化
-        bgmSlder.value = 1.0f;
-
-        // valueを初期化
-        seSlider.value = 1.0f;
-
-        // Sliderの値が変わった時のイベント登録
-        bgmSlder.onValueChanged.AddListener(OnBGMVolumeChanged);
-
-        // Sliderの値が変わった時のイベント登録
-        seSlider.onValueChanged.AddListener(OnSEVolumeChanged);
     }
 
-    private void OnBGMVolumeChanged(float value)
+    public void ChangBGMVolume()
     {
-        // ここに処理を追加
+        soundjson.UpdateVolume("BGM", bgmSlider.value);
     }
 
-    private void OnSEVolumeChanged(float value)
+    public void ChangSEVolume()
     {
-        // ここに処理を追加
+        soundjson.UpdateVolume("SE", seSlider.value);
     }
 }
